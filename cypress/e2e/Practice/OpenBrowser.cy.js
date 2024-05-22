b0cb854b0bf9fe3dbd5230796c4c765b561a54a8
Rajat725 <imrajat108@gmail.com>
2024-05-22 09:32:51 +0530

    Added Tests in OpenBrowser Spec

--- cypress/e2e/Practice/OpenBrowser.cy.js
+++ cypress/e2e/Practice/OpenBrowser.cy.js
@@ -1,11 +1,24 @@
-/// <reference types="Cypress" />
-describe('My First Test', () => {
-    it('Visits the Kitchen Sink', () => {
-      cy.visit('https://example.cypress.io');
-      cy.title().should('include', 'Kitchen Sink');
+
+describe('Open Browser', () => {
+    it('Basic Actions', () => {
+      const tester={timeout:30000};
      
+      cy.visit('https://automationexercise.com/',tester);
+      cy.visit('',{auth:{username:'',password:''}})
+      cy.get('.nav.navbar-nav li:nth-child(4)').click();
+      cy.get("input[data-qa='login-email']").type('Rajatshrma');
+      cy.get("input[placeholder='Password']").type("Indigo@123");
+      cy.get("button[data-qa='login-button']").click();
+      
+    })
+
+    it.only('Visit Site',()=>{
+
+      cy.visit('https://www.makemytrip.com/',
+      {headers:{"Accept-Encoding": "gzip, deflate"}});
+  
       
       
-      
+
     })
   })
