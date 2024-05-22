e4f86dc5a986f2892fed1095c9c5d4dd4658eebe
Rajat725 <imrajat108@gmail.com>
2024-05-22 09:33:06 +0530

    Added Spec Locator

---
+++ cypress/e2e/Practice/Locator.cy.js
@@ -0,0 +1,12 @@
+
+describe('Locators Actions', () => {
+    it('Locators Tests', () => {
+     
+      cy.visit('https://omayo.blogspot.com/',{timeout:7000});
+      
+     
+      
+    })
+
+  
+  })
