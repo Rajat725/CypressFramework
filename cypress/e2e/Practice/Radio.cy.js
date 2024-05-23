c3d6dcd06fa7765f80c60b8baecb6be8e733b468
Rajat725 <imrajat108@gmail.com>
2024-05-23 09:41:05 +0530

    Added Radio Tests

---
+++ cypress/e2e/Practice/Radio.cy.js
@@ -0,0 +1,15 @@
+describe('Radio Buttons Suites',()=>{
+    it('Radio Button Tests-1',()=>{
+        cy.visit('https://testautomationpractice.blogspot.com/',{timeout:10000});
+        cy.title().should('eq','Automation Testing Practice');
+        cy.get('#male').as('Male');
+        cy.get('#female').as('Female')
+        cy.get('@Male').check();
+        cy.get('@Male').should('be.checked');
+        cy.get('@Female').check();
+        cy.get('@Female').should('be.checked');
+        cy.get('@Male').should('not.be.checked');
+        cy.get('.form-check.form-check.form-check-inline').check();
+
+    })
+})
